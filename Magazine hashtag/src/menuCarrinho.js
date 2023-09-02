fdfef0dd83d018570aefb42de75e989d6b5251e2
Manuelly Rodrigues Victor <107930253+Manuelly1@users.noreply.github.com>
2023-09-01 22:50:30 -0300

    parte inicial da aula 4

--- Magazine hashtag/src/menuCarrinho.js
+++ Magazine hashtag/src/menuCarrinho.js
@@ -1,5 +1,7 @@
 import { catalogo } from "./utilidades";
 
+const idsProdutoCarrinhoComQuantidade = {};
+
 function abrirCarrinho() {
   document.getElementById("carrinho").classList.remove("right-[-360px]");
   document.getElementById("carrinho").classList.add("right-[0px]");
@@ -18,7 +20,27 @@ export function inicializarCarrinho() {
   botaoAbrirCarrinho.addEventListener("click", abrirCarrinho);
 }
 
+function incrementarQuantidadeProduto(idProduto) {
+  idsProdutoCarrinhoComQuantidade[idProduto]++;
+  atualizarInformacaoQuantidade(idProduto);
+}
+
+function decrementarQuantidadeProduto(idProduto) {
+  idsProdutoCarrinhoComQuantidade[idProduto]--;
+  atualizarInformacaoQuantidade(idProduto);
+}
+
+function atualizarInformacaoQuantidade(idProduto) {
+  document.getElementById(`quantidade-${idProduto}`).innerText = idsProdutoCarrinhoComQuantidade[idProduto];
+}
+
 export function adicionarAoCarrinho(idProduto) {
+  if (idProduto in idsProdutoCarrinhoComQuantidade) {
+      incrementarQuantidadeProduto(idProduto);
+      return;
+  }
+
+  idsProdutoCarrinhoComQuantidade[idProduto] = 1;
   const produto = catalogo.find(p => p.id === idProduto);
   const containerProdutosCarrinho = document.getElementById("produtos-carrinho");
 
@@ -29,14 +51,18 @@ export function adicionarAoCarrinho(idProduto) {
     <img 
       src="./assets/img/${produto.imagem}" 
       alt="Carrinho: ${produto.nome}" 
-      class="h-24 rounded-lg"
+      class="h-24 rounded-lg" 
     />
-    <div class="py-2">
+    <div class="p-2 flex flex-col justify-between">
       <p class="text-slate-900 text-sm">${produto.nome}</p>
       <p class="text-slate-400 text-xs">Tamanho: M</p>
       <p class="text-green-700 text-lg">$${produto.preco}</p>
     </div>
+    <div class="flex text-slate-950 items-end absolute bottom-0 right-2 text-lg">
+      <button>-</button>
+      <p id="quantidade-${produto.id}" class="ml-2">${idsProdutoCarrinhoComQuantidade[produto.id]}</p>
+      <button class="ml-2">+</button>
+    </div>
   </article>`;
-
   containerProdutosCarrinho.innerHTML += cartaoProdutoCarrinho;
 }
